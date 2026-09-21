c33fcb6625376fdc0d9ae4b323d49a1f6a64e09c
kennethriemer <kamminkenneth8@gmail.com>
2026-09-22 00:02:06 +0200

    Update admin-sw.js

--- admin-sw.js
+++ admin-sw.js
@@ -1,7 +1,62 @@
+const CACHE = "riemer-admin-v2";
+
+const ASSETS = [
+  "./admin.html",
+  "./admin-manifest.webmanifest",
+  "./icon.svg"
+];
+
 self.addEventListener("install", event => {
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  const request = event.request;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  const url = new URL(request.url);
+
+  if (url.pathname.endsWith("/admin.html")) {
+    event.respondWith(
+      fetch(request)
+        .then(response => {
+          const copy = response.clone();
+
+          caches.open(CACHE).then(cache => {
+            cache.put(request, copy);
+          });
+
+          return response;
+        })
+        .catch(() => caches.match(request))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request)
+      .then(cached => cached || fetch(request))
+  );
 });
